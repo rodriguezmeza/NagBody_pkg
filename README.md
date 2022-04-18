d97a71c79947fd718263d2a4a94157e8131b45f6
rodriguezmeza <marioalberto.rodriguez@inin.gob.mx>
2022-04-18 14:00:35 -0500

    Update env_config and README.md

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
 # NagBody_pkg
 Numerical algoritms for general body dynamics
 
+In your home:
+
 git clone https://github.com/rodriguezmeza/NagBody_pkg.git
+
+Installation:
+
+$ cd $HOME/NagBody_pkg
+
+It is assumed that the trajectory to NagBody_pkg is under the your home directory. Then
+
+$ make -f NagBody install_dirs
+
+to make directories: bin, local, man, tests and zip. Let us do
+
+$ make -f NagBody install_nbody_n2
+
+Then move to tests:
+
+$ cd tests
+
+and run:
+
+$ ../bin/nbody_n2
+
+To get help:
+
+$ ../bin/nbody_n2 -help
+
+or
+
+$ man ../man/man1/nbody_n2.1
+
+and return to NagBody_pkg dir:
+
+$ cd ..
+
+Ok. Now let source:
+
+$ source env_config/nagbodyrc.sh
+
+Go again to tests directory and do the above tests:
+
+$ nbody_n2
+$ nbody_n2 -help
+$ man nbody_n2
+
+You may see that now you do not need to add ../bin, etc. That is because in the env_config/nagbodyrc.sh there are some additions to the environment variables and you activated by sourcing it.
+
 
 
---
+++ env_config/nagbodyrc.sh
@@ -0,0 +1,77 @@
+# this code cannot be run directly
+# do 'source env_config/nagbodyrc.sh' from your sh shell or put it in your profile
+#
+#
+
+#
+#To solve some configuration problems related to zsh visit:
+#https://scriptingosx.com/2019/06/moving-to-zsh-part-2-configuration-files/
+#
+#
+# NagBody Defintion of variables ...
+
+# To activate nnbodykit environment (added to load notebooks in jupyter, doesnot work)
+#source /anaconda2/bin/activate /anaconda2/envs/nbodykit-env/
+
+# NagBody:
+export PATH=${HOME}/NagBody_pkg/bin:${PATH}
+export MANPATH=${HOME}/NagBody_pkg/man:${MANPATH}
+
+# FFTW-2:
+# INSTALADO CON PORT ... fftw, fftw-single, falto que se incluya con openmpi ...
+#export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw2/lib:${DYLD_LIBRARY_PATH}
+
+# CFITSIO (NEEDED BY CAMB):
+export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/cfitsio/lib/pkgconfig:${PKG_CONFIG_PATH}
+export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/cfitsio/lib:${DYLD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/cfitsio/lib:${LD_LIBRARY_PATH}
+
+# LAPACK (Instalada con ports en Mac):
+#export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/lapack/lib:${LD_LIBRARY_PATH}
+
+# GSL:
+# INSTALADO CON PORT ...
+#export PATH=${HOME}/NagBody_pkg/local/gsl/bin:${PATH}
+#export MANPATH=${HOME}/NagBody_pkg/local/gsl/share/man:${MANPATH}
+#export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/gsl/lib/pkgconfig:${PKG_CONFIG_PATH}
+#export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/gsl/lib:${LD_LIBRARY_PATH}
+#
+
+# FFTW3:
+# Instalar con port ... fftw-3 ... tambien la precision simple fftw-3-single
+#export PATH=${HOME}/NagBody_pkg/local/fftw3/bin:${PATH}
+#export MANPATH=${HOME}/NagBody_pkg/local/fftw3/share/man:${MANPATH}
+#export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/fftw3/lib/pkgconfig:${PKG_CONFIG_PATH}
+#export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw3/lib:${DYLD_LIBRARY_PATH}
+#export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw3/lib:${LD_LIBRARY_PATH}
+
+###################################################################
+# HDF5:
+# INSTALADO CON PORT ... hdf5-18 ...
+#export PATH=${HOME}/NagBody_pkg/local/hdf5/bin:${PATH}
+#export MANPATH=${HOME}/NagBody_pkg/local/hdf5/share/man:${MANPATH}
+#export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/hdf5/lib:${LD_LIBRARY_PATH}
+#export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/hdf5/lib:${DYLD_LIBRARY_PATH}
+
+# PHDF5 (Parallel):
+#export PATH=${HOME}/NagBody_pkg/local/phdf5/bin:${PATH}
+#export MANPATH=${HOME}/NagBody_pkg/local/phdf5/share/man:${MANPATH}
+#export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/phdf5/lib:${LD_LIBRARY_PATH}
+#export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/phdf5/lib:${DYLD_LIBRARY_PATH}
+
+
+# GADGETVIEWER:
+export PATH=${HOME}/NagBody_pkg/local/gadgetviewer/bin:${PATH}
+
+###################################################################
+# Planck 2018:
+# To activate Planck 2018 for CLASS:
+#source $HOME/NagBody_pkg/NagBody_sources/class/planck2018/src/code/plc_3.0/plc-3.01/bin/clik_profile.sh
+source $HOME/NagBody_pkg/bin/clik_profile.sh
+
+
+
+###################################################################
+# Python environment:
+export PYTHONPATH=${HOME}/.local/lib/python3.9/site-packages:${PYTHONPATH}
+
