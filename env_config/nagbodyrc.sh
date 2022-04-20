845878137024f70bc4c0bb8fa2f594114afb011a
rodriguezmeza <marioalberto.rodriguez@inin.gob.mx>
2022-04-20 10:46:17 -0500

    Update nagbodyrc.sh

--- env_config/nagbodyrc.sh
+++ env_config/nagbodyrc.sh
@@ -10,40 +10,48 @@
 #
 # NagBody Defintion of variables ...
 
-# To activate nnbodykit environment (added to load notebooks in jupyter, doesnot work)
+# To activate nnbodykit environment (added to load notebooks in jupyter, does not work)
 #source /anaconda2/bin/activate /anaconda2/envs/nbodykit-env/
 
 # NagBody:
 export PATH=${HOME}/NagBody_pkg/bin:${PATH}
 export MANPATH=${HOME}/NagBody_pkg/man:${MANPATH}
 
-# FFTW-2:
-# INSTALADO CON PORT ... fftw, fftw-single, falto que se incluya con openmpi ...
-#export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw2/lib:${DYLD_LIBRARY_PATH}
-
 # CFITSIO (NEEDED BY CAMB):
 export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/cfitsio/lib/pkgconfig:${PKG_CONFIG_PATH}
 export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/cfitsio/lib:${DYLD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/cfitsio/lib:${LD_LIBRARY_PATH}
 
-# LAPACK (Instalada con ports en Mac):
-#export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/lapack/lib:${LD_LIBRARY_PATH}
-
-# GSL:
-# INSTALADO CON PORT ...
-#export PATH=${HOME}/NagBody_pkg/local/gsl/bin:${PATH}
-#export MANPATH=${HOME}/NagBody_pkg/local/gsl/share/man:${MANPATH}
-#export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/gsl/lib/pkgconfig:${PKG_CONFIG_PATH}
-#export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/gsl/lib:${LD_LIBRARY_PATH}
-#
+# FFTW-2:
+# INSTALADO CON PORT ... fftw, fftw-single, falto que se incluya con openmpi ...
+export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw2/lib:${DYLD_LIBRARY_PATH}
 
 # FFTW3:
 # Instalar con port ... fftw-3 ... tambien la precision simple fftw-3-single
-#export PATH=${HOME}/NagBody_pkg/local/fftw3/bin:${PATH}
-#export MANPATH=${HOME}/NagBody_pkg/local/fftw3/share/man:${MANPATH}
-#export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/fftw3/lib/pkgconfig:${PKG_CONFIG_PATH}
-#export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw3/lib:${DYLD_LIBRARY_PATH}
-#export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw3/lib:${LD_LIBRARY_PATH}
+export PATH=${HOME}/NagBody_pkg/local/fftw3/bin:${PATH}
+export MANPATH=${HOME}/NagBody_pkg/local/fftw3/share/man:${MANPATH}
+export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/fftw3/lib/pkgconfig:${PKG_CONFIG_PATH}
+export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw3/lib:${DYLD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/fftw3/lib:${LD_LIBRARY_PATH}
+
+# GSL:
+# INSTALADO CON PORT ...
+export PATH=${HOME}/NagBody_pkg/local/gsl/bin:${PATH}
+export MANPATH=${HOME}/NagBody_pkg/local/gsl/share/man:${MANPATH}
+export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/gsl/lib/pkgconfig:${PKG_CONFIG_PATH}
+export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/gsl/lib:${LD_LIBRARY_PATH}
+#
+
+# LAPACK (Instalada con ports en Mac):
+export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/lapack/lib:${LD_LIBRARY_PATH}
+
+###################################################################
+# PLPlot (ver. 5.15.0):
+export PATH=${HOME}/NagBody_pkg/local/plplot/bin:${PATH}
+export MANPATH=${HOME}/NagBody_pkg/local/plplot/share/man:${MANPATH}
+export PKG_CONFIG_PATH=${HOME}/NagBody_pkg/local/plplot/lib/pkgconfig:${PKG_CONFIG_PATH}
+export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/plplot/lib:${DYLD_LIBRARY_PATH}
+export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/plplot/lib/plplot5.15.0/driversd:${DYLD_LIBRARY_PATH}
 
 ###################################################################
 # HDF5:
@@ -59,7 +67,6 @@ export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/cfitsio/lib:${LD_LIBRARY_PATH}
 #export LD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/phdf5/lib:${LD_LIBRARY_PATH}
 #export DYLD_LIBRARY_PATH=${HOME}/NagBody_pkg/local/phdf5/lib:${DYLD_LIBRARY_PATH}
 
-
 # GADGETVIEWER:
 export PATH=${HOME}/NagBody_pkg/local/gadgetviewer/bin:${PATH}
 
@@ -72,6 +79,6 @@ source $HOME/NagBody_pkg/bin/clik_profile.sh
 
 
 ###################################################################
-# Python environment:
+# Python environment (ver 3.9):
 export PYTHONPATH=${HOME}/.local/lib/python3.9/site-packages:${PYTHONPATH}
 
